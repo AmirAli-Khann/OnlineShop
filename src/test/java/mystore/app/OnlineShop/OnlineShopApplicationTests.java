2661da69234cb069cdd4933e792fb76a3a340b44
Amir AliFakhrabadi <amir.a.fakhrabadi@gmail.com>
2024-05-03 13:56:55 +0200

    Initial commit

---
+++ src/test/java/mystore/app/OnlineShop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mystore.app.OnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
